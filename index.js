f5cedfbbfda6e02874143e36182e9d83ad3ae1fc
alikonuk12 <alikonuk12@gmail.com>
2022-07-11 23:13:41 +0300

    add cors config

--- index.js
+++ index.js
@@ -2,17 +2,34 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT;
 const hostname = process.env.YOUR_HOST;
 const database = process.env.DATABASE;
 const API = require('./api');
 
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(null, false);
+        }
+    },
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    optionsSuccessStatus: 200,
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+};
+
 mongoose.connect(database);
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use('/api', API);
 
 app.get('/', (req, res) => {
