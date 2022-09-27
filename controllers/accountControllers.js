75f9ff6f4a670018ae7aa2db8b170680cb44ec13
alikonuk12 <alikonuk12@gmail.com>
2022-09-28 00:58:44 +0300

    change cookieOptions

--- controllers/accountControllers.js
+++ controllers/accountControllers.js
@@ -16,7 +16,9 @@ const createSendToken = async (account, res) => {
 
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        httpOnly: true,
+		secure: true,
+        sameSite: 'none'
     };
 
     res.cookie('jwt', token, cookieOptions);
