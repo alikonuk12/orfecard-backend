4ebc5fefbf4f625bd328c379480c8c452e020d61
alikonuk12 <alikonuk12@gmail.com>
2022-09-27 23:54:45 +0300

    remove secure field in cookieOptions

--- controllers/accountControllers.js
+++ controllers/accountControllers.js
@@ -16,8 +16,7 @@ const createSendToken = async (account, res) => {
 
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production'
+        httpOnly: true
     };
 
     res.cookie('jwt', token, cookieOptions);
